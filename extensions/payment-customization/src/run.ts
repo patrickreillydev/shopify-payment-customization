8ec19b82545b0b8bfba3fbf3e7f6522cd0c8a92f
patrickreillydev <79639852+patrickreillydev@users.noreply.github.com>
2024-06-26 17:45:22 +1000

    Update run.ts

--- extensions/payment-customization/src/run.ts
+++ extensions/payment-customization/src/run.ts
@@ -19,8 +19,13 @@ const HIDE_PAYMENT_METHOD = 'afterpay';
 const UNIQUE_PRODUCT_COUNT = 1
 
 export function run(input: RunInput): FunctionRunResult {
-  const hidePaymentMethod = input.cart.deliverableLines.length > UNIQUE_PRODUCT_COUNT
-  const paymentMethodToHide = input.paymentMethods.find(p => p?.name?.toLowerCase() === HIDE_PAYMENT_METHOD)
+
+  if (!input.cart || !input?.paymentMethods) {
+    return NO_CHANGES;
+  }
+
+  const hidePaymentMethod = input?.cart?.deliverableLines?.length > UNIQUE_PRODUCT_COUNT
+  const paymentMethodToHide = input?.paymentMethods?.find(p => p?.name?.toLowerCase() === HIDE_PAYMENT_METHOD)
   if (hidePaymentMethod && paymentMethodToHide) {
     return {
       operations: [{
